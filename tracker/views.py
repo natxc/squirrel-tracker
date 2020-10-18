edb102bfe8c9825889653802fe730ae0f124fbd0
John Kim <jjk2247@columbia.edu>
2020-10-18 06:41:49 +0000

    Fixed map views to include .html and started on sightings.

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from tracker.models import Squirrel
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def index(request):
@@ -7,5 +8,14 @@ def index(request):
 
 def map(request):
     squirrels = Squirrel.objects.all()[:100]
-    context = {'squirrels':squirrels}
+    context = {
+            'squirrels': squirrels
+        }
     return render(request, 'tracker/map.html', context)
+
+def sightings(request):
+    squirrles = Squirrel.objects.all()
+    context = {
+            'squirrels': squirrels
+            }
+    return render(request, 'tracker/sightings.html', context)

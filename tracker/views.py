40eb9213fd1bebd62de72424170e7afa3fac8a11
John Kim <jjk2247@columbia.edu>
2020-10-18 05:26:33 +0000

    Added a views function for map.

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from tracker.models import Squirrel
+
 
 def index(request):
     return render(request, 'tracker/index.html', {})
+
+def map(request):
+    squirrels = Squirrel.objects.all()[:100]
+    context = {'squirrels':squirrels}
+    return render(request, 'tracker/map.html', context)

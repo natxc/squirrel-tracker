ac6550e9b4c06461855386c9db9d8fccf7474088
John Kim <jjk2247@columbia.edu>
2020-10-18 17:34:08 +0000

    Fixed a typo.

--- tracker/urls.py
+++ tracker/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
         path('', views.index),
         path('map.html', views.map, name='map'),
         path('sightings.html', views.sightings, name='sightings'),
+        path('sightings/add', views.add, name='add'),
+        path('sightings/stats', views.stats, name='stats'),
 ]
